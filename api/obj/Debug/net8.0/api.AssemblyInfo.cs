e4f09e2ab7c22a2ebb7e28a73e10b5a940a2a4ae
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-30 16:31:19 +0100

    Created Models folder with basic classes: Stock and Comment

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dadeb8fd2c14a116dadf52390928a4ccd412118")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99df2900e5cd5c3b3ce2c171102a28d08aaefb2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
