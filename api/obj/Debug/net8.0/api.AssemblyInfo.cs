9d32966875285aa59e6dc796b53c4b12c8f0de9f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-02 11:48:10 +0100

    Created StockDto.cs

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f02339f22487d6a531c008d21e91e36d11e2393")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fa6f46ef39d01478ea088b6505c0f74ba8bf19a")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
