a04d1629b31410b48bfdd7e549d433100abe29b5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-03 10:47:02 +0100

    Updated IStockRepository

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f2de9a6bec103953797bcc8b8ce5d63ad1bc2fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0da77a0b069bf506df54ec8b84f478817e83b53f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
