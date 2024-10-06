3537599d1e5eea0357b02d769747dade3766cff6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-06 19:18:27 +0100

    Created CommentRepository.cs

---
+++ api/Controllers/CommentController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepo;
+
+        public CommentController(ICommentRepository commentRepo)
+        {
+            _commentRepo = commentRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comment = await _commentRepo.GetAllAsync();
+        }
+    }
+}
