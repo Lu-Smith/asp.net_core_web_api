3e1d5f3efd229ab4da59d7dd3bfa3e30b73081ba
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-07 13:23:14 +0100

    Installed new packages

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96801971bb6e63ab03905ba913b79e0de6f3ab45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3c6559aaba2973bb5d07a646d2bf3e1021d1b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
